90556dea90a75f1d15346c16fff072eb68080a3e
songhao <songhao@songhaodeMacBook-Pro.local>
2017-07-04 16:20:39 +0800

    add spec

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "可以把商品丢到购物车里，然后购物车里就有东西了" do
+
+  end
+
+  it "如果加了相同种类的商品到购物车里，购买项目(CartItem)并不会增加，但商品的数量会改变" do
+
+  end
+
+  it "商品可以放到购物车里，也可以再拿出来" do
+
+  end
+
+  it "每个CartItem都可以计算自己的金额(小计)" do
+
+  end
+
+  it "可以计算整台购物车的总消费金额" do
+
+  end
+
+  it "特别活动可能可搭配优惠活动(例如新年全场九折，或是满金额免运费)" do
+
+  end
 end
